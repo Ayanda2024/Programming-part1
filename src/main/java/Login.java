8b6d903e9ee0d32921af626e2e813aff8611d7ef
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-09-27 21:07:34 +0000

    Second commit

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -16,4 +16,16 @@ public class Login {
         String enteredusername;
         String enteredpassword;
 
+         public boolean checkusername(String enteredusername){
+    //ensuring that the entered username is formatted correctly
+            if (enteredusername.contains("_")&&enteredusername.length()<=5){
+              System.out.println( "Username successfully captured" );
+               
+               return true;
+            }
+            else{
+                System.out.println("Username is not correctly formatted ,please ensure that your username contains an underscore and is no more than 5 characters in length");
+                return false;
+            }
+}
 }
