a52f83674c81ac4c5b9e36f332b82b469d193dbd
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-09-27 23:20:38 +0000

    Second commit

---
+++ src/test/java/com/mycompany/phase1/programming/LoginTest.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.phase1.programming;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class LoginTest {
+    Login my;
+    public LoginTest() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+        my= new Login();
+    }
+    
+    @AfterEach
+    public void tearDown() {
+        my=null;
+    }
+
+    @Test
+    public void testCheckusername() {
+        //Validate username input validation for Empty string
+        System.out.print("checkusername");
+        String enteredusername="";
+        boolean expResult=false;  //The system shall return a value of false for any empty username
+        boolean result=my.checkusername(enteredusername);
+        
+        assertEquals(expResult,result);//Confirm actual result matches expected outcome
+        
+    }
+    
+}
