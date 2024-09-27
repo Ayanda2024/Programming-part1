782b62f633ce253a11a4611ce060c8260f45d8d5
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-09-27 20:10:10 +0000

    first commit

---
+++ src/main/java/Login.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Login {
+    //Declarations
+        String username="Aya_1";
+        String password="Berry@1992!";
+        String firstname="Ayanda";
+        String lastname="Ngwenya";
+        String enteredusername;
+        String enteredpassword;
+
+}
---
+++ src/main/java/com/mycompany/phase1/programming/Phase1Programming.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.phase1.programming;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Phase1Programming {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
