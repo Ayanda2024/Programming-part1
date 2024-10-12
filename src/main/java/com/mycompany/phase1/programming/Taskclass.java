826eabff7ed1c94b6352b9418ca5ef23b2a689be
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-10-12 19:23:09 +0000

    Declared variables for Task class

---
+++ src/main/java/com/mycompany/phase1/programming/Taskclass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.phase1.programming;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Taskclass {
+    private String WelcomeMessage;
+    private String TaskName;
+    private String TaskNumber;
+    private String TaskDescription;
+    private String DeveloperDetails;
+    private double Taskduration;
+    private String TaskID;
+    private String TaskStatus;
+    
+            
+}
